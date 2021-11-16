b8f1cf87a68704c8bb69415f4f0418465a0e1eb1
emirgundem <emirgndem@gmail.com>
2021-11-16 12:17:06 +0300

    Js Objects

---
+++ JsObjects/object.js
@@ -0,0 +1,35 @@
+
+//In JavaScript, objects are king. If you understand objects, you understand JavaScript.
+//Javascripte nesneler kraldır eğer nesneleri anlarsanız,javascripti anlarsınız.
+
+
+/*
+In JavaScript, almost "everything" is an object.
+
+Booleans can be objects (if defined with the new keyword)
+Numbers can be objects (if defined with the new keyword)
+Strings can be objects (if defined with the new keyword)
+Dates are always objects
+Maths are always objects
+Regular expressions are always objects
+Arrays are always objects
+Functions are always objects
+Objects are always objects
+All JavaScript values, except primitives, are objects.
+
+*/
+
+const person = {
+    firstName : "emir",
+    lastName : "gündem",
+    age : 25,
+    email : "emirgndem@gmail.com" 
+}
+
+const google = {
+    googleKey : "testkey"
+}
+
+c
+console.log(person);
+console.log(person1.googleKey);
